271e08cd8af85fe1179257115d7f13adbb9ce137
David Morant <davidm@touchlab.co>
2017-11-14 19:32:29 -0500

    Refactoringgg

---
+++ iosApp/Pods/Target Support Files/doppllib/doppllib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_doppllib : NSObject
+@end
+@implementation PodsDummy_doppllib
+@end
---
+++ iosApp/iosApp/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  iosApp
+//
+//  Created by touchlab on 11/13/17.
+//  Copyright © 2017 touchlab. All rights reserved.
+//
+
+import UIKit
+import doppllib
+
+class ViewController: UIViewController, UITableViewDataSource, SOAQuestionsViewModel_Host {
+    
+    private var questions = [SOAQuestion]()
+    
+    func setQuestionsWith(_ questions: JavaUtilList!) {
+        self.questions = (JavaUtils.javaList(toList: questions) as? [SOAQuestion])!
+        tableView.reloadData()
+    }
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let viewModel = SOAQuestionsViewModel()
+        viewModel?.register__(with: self)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let question: SOAQuestion = questions[indexPath.row]
+        cell.textLabel?.text = question.title_
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questions.count
+    }
+}
+
+class RxJConsumer : NSObject, IoReactivexFunctionsConsumer {
+    func accept(withId t: Any!) {
+        
+    }
+    
+    
+    
+    
+}
