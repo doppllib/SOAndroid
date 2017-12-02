3d6dfa84d848e0ea4a741d47beeab88c0b76eb89
Kevin Galligan <kgalligan@gmail.com>
2017-12-02 07:10:50 -0500

    build stuff added

---
+++ iosTest/iosTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosTest
+//
+//  Created by Kevin Galligan on 12/1/17.
+//  Copyright © 2017 doppl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
